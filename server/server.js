543f2da20240f899e6e6cdc33048d89908de8026
Paul Borawski <paulborawski14@gmail.com>
2018-03-13 08:43:12 -0700

    using the data from gdax api and updating it every five seconds on localhost:7777/results when running server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,30 @@
 const port = 7777;
 const app = require('express')();
+const request = require('request');
+
+const url = "https://api.gdax.com/products/BTC-USD/ticker";
+
+var btcusd = {};
+
+request({url: 'https://api.gdax.com/products/BTC-USD/ticker', headers:{'user-agent':'eatme'}}, function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+   console.log('body:', body); // Print the HTML for the Google homepage.
+   btcusd.body = JSON.parse(body)
+});
+
+setInterval(function() {
+  request({url: 'https://api.gdax.com/products/BTC-USD/ticker', headers:{'user-agent':'eatme'}}, function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+     console.log('body:', body); // Print the HTML for the Google homepage.
+     btcusd.body = JSON.parse(body)
+  });
+}, 5000)
+
 
 app.get('/results', function (req, res) {
-  res.send('hello');
+  res.send(btcusd.body);
 });
 
 require('http').createServer(app).listen(port,function(){
