24a1df8554d3f5d39554a74c5a0cb011b08faedb
Paul Borawski <paulborawski14@gmail.com>
2018-03-13 08:17:42 -0700

    BASIC CONTAINER THAT UPDATES STATE

--- src/containers/ContainerOne.js
+++ src/containers/ContainerOne.js
@@ -1,6 +1,28 @@
-/*import React, { Component } from 'react';
+import React, { Component } from 'react';
 
-class React extends Component;
+class ContainerOne extends Component {
+state = {
+    stateone: {
+       one: 0,
+       two: 0
+    },
+    statetwo: 0,
+    statethree: 0,
+    statefour: 0
+  }
 
+  stateSetter = () => {
+    this.setState({statefour: 4});
+  }
+
+  render () {
+    return (
+      <div>
+       <h1>"Is this working?"</h1>
+       <h1>{this.state.statefour}</h1>
+       <button onClick={this.stateSetter}>Click me to update state</button>
+      </div>
+    )
+  }
+}
 export default ContainerOne;
-*/
